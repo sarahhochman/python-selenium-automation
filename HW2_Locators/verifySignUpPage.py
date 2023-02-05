902daa116daa7e7ca5ba0eec0de3dbe4c1ef6bbf
Sarah Hochman <morahhochman@gmail.com>
2023-02-05 17:55:53 -0500

    check for sign up page

---
+++ HW2_Locators/verifySignUpPage.py
@@ -0,0 +1,29 @@
+git from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+from selenium.webdriver.chrome.service import Service
+
+# driver = webdriver.Chrome(executable_path='/Users/svetlanalevinsohn/JobEasy/13-python-selenium-automation/chromedriver')
+service = Service('/Users/svetlanalevinsohn/JobEasy/13-python-selenium-automation/chromedriver')
+driver = webdriver.Chrome(service=service)
+
+driver.get('https://www.amazon.com/')
+
+driver.find_element(By.ID, 'nav-link-accountList-nav-line-1').click()
+expected_result = 'Sign in'
+actual_result = driver.find_element(By.XPATH, "//h1[@class='a-spacing-small']").text
+
+assert expected_result == actual_result, f'Expected {expected_result} but got actual {actual_result}'
+print('Test Passed!')
+driver.quit()
+
+
+
+
+
+
+
+
+
+
+
